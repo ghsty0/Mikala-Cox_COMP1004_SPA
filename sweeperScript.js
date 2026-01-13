952afab01f23e6fcaa361ee215f4b868dc8cd121
Mikala Cox <macox06mc@gmail.com>
2026-01-13 17:09:47 +0000

    small CSS changes, beginning js

--- sweeperScript.js
+++ sweeperScript.js
@@ -1,3 +1,29 @@
 //Script for the game.
-window.alert("Hello World!")
-console.log("haiii :33")
+let HEIGHT = 0;
+let WIDTH = 0;
+let catCount = 0;
+let D = 1;
+
+const gameboard = document.getElementById("gameboard");
+
+function difficulty(){
+    switch (D){
+        case 1:
+            HEIGHT = 8;
+            WIDTH = 8;
+            catCount = 10;
+            break;
+        case 2:
+            HEIGHT = 10;
+            WIDTH = 10;
+            catCount = 12;
+            break;
+        case 3:
+            HEIGHT = 12;
+            WIDTH = 12;
+            catCount = 15;
+            break;
+    }
+}
+
+
